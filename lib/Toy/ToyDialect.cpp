459cdf9fa5e6bcac5a968b241b958d195c47699c
Hongze Zhao <hzhao@nuro.ai>
2022-10-29 12:20:17 -0700

    Fix accessor prefix issue

--- lib/Toy/ToyDialect.cpp
+++ lib/Toy/ToyDialect.cpp
@@ -9,11 +9,16 @@
 using namespace mlir;
 using namespace toy;
 
-
-
 void ToyDialect::initialize() {
   addOperations<
 #define GET_OP_LIST
 #include "Toy/ToyOps.cpp.inc"
   >();
+}
+
+void FuncOp::build(mlir::OpBuilder &builder, mlir::OperationState &state,
+                   llvm::StringRef name, mlir::FunctionType type,
+                   llvm::ArrayRef<mlir::NamedAttribute> attrs)
+{
+  buildWithEntryBlock(builder, state, name, type, attrs, type.getInputs());
 }
--- lib/Toy/ToyOps.cpp
+++ lib/Toy/ToyOps.cpp
@@ -1,5 +1,7 @@
 #include "Toy/ToyOps.h"
 #include "Toy/ToyDialect.h"
 
+using namespace mlir;
+
 #define GET_OP_CLASSES
 #include "Toy/ToyOps.cpp.inc"
